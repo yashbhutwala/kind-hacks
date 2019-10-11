6561fe70740d5ae0da9de6b8ec8c4f0fa43c2f95
Yash Bhutwala <ymb002@bucknell.edu>
2019-10-11 11:09:08 -0400

    add featureGate for TTL Controller

--- setup-kind.sh
+++ setup-kind.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-export KIND_LOCATION="~/kind-hacks"
-export KUBE_CONFIG_LOCATION="~/.kube"
-export KIND_CLUSTER_NAME="${KIND_CLUSTER_NAME}"
+export KUBE_CONFIG_LOCATION="/Users/bhutwala/.kube"
+export KIND_CLUSTER_NAME="kind-v1.15.3-multi-node-cluster"
 
 # create the kind cluster
-kind create cluster --config ${KIND_LOCATION}/${KIND_CLUSTER_NAME}.yml --name ${KIND_CLUSTER_NAME}
+kind create cluster --config ${KIND_CLUSTER_NAME}.yml --name ${KIND_CLUSTER_NAME}
 
 # change kubeconfig to the kind config
 cp ${KUBE_CONFIG_LOCATION}/kind-config-${KIND_CLUSTER_NAME} ${KUBE_CONFIG_LOCATION}/config
@@ -36,3 +35,9 @@ ngrok http https://localhost &
 # into all the nodes manually
 
 # kind load docker-image registry.access.redhat.com/rhscl/postgresql-10-rhel7:1 --name=${KIND_CLUSTER_NAME}
+
+
+
+## TODO: cleanup
+kind delete cluster --name kind-v1.15.3-multi-node-cluster
+docker stop kind-v1.15.3-multi-node-cluster-kind-proxy-443 && docker stop kind-v1.15.3-multi-node-cluster-kind-proxy-80
